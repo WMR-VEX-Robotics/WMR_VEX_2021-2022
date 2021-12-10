3694f8f4279f1e22e394acc52d66f3c5d57b8b83
CL0520 <92118882+CL0520@users.noreply.github.com>
2021-12-10 13:09:47 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,11 @@
 // LFM                  motor         1               
 // LRM                  motor         2               
 // RFM                  motor         3               
-// RRM                  motor         4  
+// RRM                  motor         4 
+// VM                   motor         5
+// SM                   motor         6
+// FL1                  motor         7
+// FL2                  motor         8
 // LS1                  limit         A
 // LS2                  limit         B
 // ---- END VEXCODE CONFIGURED DEVICES ----
@@ -64,7 +68,7 @@ void pre_auton(void) {
 
 void autonomous(void) {
   int route;
- // route = 
+  route = (LS1.value() * 2) + (LS2.value() * 4);
 }
 
 /*---------------------------------------------------------------------------*/
