9b6b4cf2fdafa1f4e389599d9905786136e6aaae
CL0520 <cl.steven520@gmail.com>
2022-01-12 14:42:21 -0500

    yes

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -10,12 +10,12 @@ brain  Brain;
 controller Controller1 = controller(primary);
 motor LFM = motor(PORT1, ratio18_1, false);
 motor LRM = motor(PORT2, ratio18_1, false);
-motor RFM = motor(PORT3, ratio18_1, false);
-motor RRM = motor(PORT4, ratio18_1, false);
-motor AM1 = motor(PORT5, ratio18_1, false);
-motor AM2 = motor(PORT6, ratio18_1, true);
-motor CM1 = motor(PORT7, ratio6_1, false);
-motor CM2 = motor(PORT8, ratio18_1, false);
+motor RFM = motor(PORT4, ratio18_1, false);
+motor RRM = motor(PORT5, ratio18_1, false);
+motor AM1 = motor(PORT20, ratio18_1, false);
+motor AM2 = motor(PORT18, ratio18_1, true);
+motor CM1 = motor(PORT19, ratio6_1, false);
+motor CM2 = motor(PORT17, ratio18_1, false);
 limit LS1 = limit(Brain.ThreeWirePort.A);
 limit LS2 = limit(Brain.ThreeWirePort.B);
 
